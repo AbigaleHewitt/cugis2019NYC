c17d495bb392eb8de971a36b72c75bcc3694639c
AbigaleHewitt <54039973+AbigaleHewitt@users.noreply.github.com>
2019-08-12 14:32:10 -0400

    Add files via upload

---
+++ Day one file.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("hello world")  #greeting
+print("19")  #number
+print(5*2)
+print(5/2)
+print(5**2)
+print(8/9*3)
