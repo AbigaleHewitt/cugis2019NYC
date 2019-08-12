4a46d29983cffad37ae6b3c8b269eeb7529235ad
AbigaleHewitt <54039973+AbigaleHewitt@users.noreply.github.com>
2019-08-12 15:13:21 -0400

    Add files via upload

--- Day one file.py
+++ Day one file.py
@@ -11,3 +11,17 @@ print(5*2)
 print(5/2)
 print(5**2)
 print(8/9*3)
+def add(a,b):
+    add =a+b
+    print(add)
+
+add(2,3)
+
+add (419,1)
+
+def add(a,b,c):
+    add = a+b+c
+    print (add)
+
+print("sum of 4387.798 and 25970.56 is..." )
+add (4387.798,25970.56)
